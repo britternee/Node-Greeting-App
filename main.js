3af6a6cd584a4b7f1bbde26344e9460348440348
Brittany Miller <britmill@gmail.com>
2018-09-02 12:15:21 -0600

    basic node app complete

---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const goodbyes = require("./goodbye.js");
+
+//"Hello"
+greetings.greetingEnglish();
+
+//"Konichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Hallo"
+greetings.greetingIcelandic();
+
+//"Goodbye"
+goodbyes.goodbyeEnglish();
+
+//"Sayonara"
+goodbyes.goodbyeJapanese();
+
+//"Adios"
+goodbyes.goodbyeSpanish();
+
+//"Bless"
+goodbyes.goodbyeIcelandic();
